cc28be65adf3f30815459131d3b6c33a4b6f43cc
MaxReX92 <maxrex@list.ru>
2015-08-21 23:55:59 +0300

    Мелкие исправления

--- VideoConference/MainForm.Designer.cs
+++ VideoConference/MainForm.Designer.cs
@@ -28,12 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 496);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+
     }
 }
 
